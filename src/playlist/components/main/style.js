b37454edb3f4b1dd9f1cb05e755efac2f96b883f
Pablo Guzmán <guzman.pablo87@gmail.com>
2019-01-07 00:54:08 -0300

    gestion de archivos base

---
+++ src/playlist/components/main/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Main = styled.main`
+	max-width: 968px;
+	margin: auto;
+	width: 96%;
+`;
+
+export default Main;
---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Theme = {
+	colors: {
+		primary: {
+			500: ''
+		},
+		secundary: {
+			500: ''
+		}
+	},
+	fonts: styled.p`
+		font-family: 'helvetica';
+		line-height: 120%;
+		font-size: 12px;
+		padding: 0;
+		margin: 0;
+		color: #3d3d3d;
+	`
+};
+
+export default Theme;
